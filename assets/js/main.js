a6ed706d456c384c34719611db8da89cf0c79484
OmarAliSoliman <omarasmar9955@gmail.com>
2020-09-19 15:12:03 +0200

    final-version

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,44 +7,53 @@ $(document).ready(function () {
     })
   }
 
-  // if($('.consult-slider').length){
-  //   $('.consult-slider').slick({
-  //     dots: false,
-  //     infinite: false,
-  //     speed: 300,
-  //     slidesToShow: 4,
-  //     slidesToScroll: 1,
-  //     autoplay: false,
-  //     loop: true,
-  //     infinite: true,
-  //     responsive: [
-  //       {
-  //         breakpoint: 1024,
-  //         settings: {
-  //           slidesToShow: 3,
-  //           slidesToScroll: 3,
-  //           infinite: true,
-  //           dots: true
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 800,
-  //         settings: {
-  //           slidesToShow: 2,
-  //           slidesToScroll: 2
-  //         }
-  //       },
-  //       {
-  //         breakpoint: 480,
-  //         settings: {
-  //           slidesToShow: 1,
-  //           slidesToScroll: 1
-  //         }
-  //       }
-  //     ]
-  //   });
-  // }
+  if($('.add-slider').length){
+    $('.add-slider').slick({
+      dots: true,
+      arrows: false
+    })
+  }
 
+
+
+  if($('.related-house-add-slider').length){
+    $('.related-house-add-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      loop: true,
+      infinite: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  }
 
 
 
